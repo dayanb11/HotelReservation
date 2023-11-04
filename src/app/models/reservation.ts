051a4702ac75d4a3b3dcc22f567ac555330bc469
dayanb11 <dayanbeni@gmail.com>
2023-11-04 17:57:14 +0200

    commit for roey

--- src/app/models/reservation.ts
+++ src/app/models/reservation.ts
@@ -1,8 +1,18 @@
-export interface Reservation {
-    id: string,
-    checkInDate: Date,
-    checkOutDate: Date,
-    guestName: string,
-    guestEmail: string,
-    roomNumber: number
+export class Reservation {
+    public id: string;
+    public checkInDate: Date;
+    public checkOutDate: Date;
+    public guestName: string;
+    public guestEmail: string;
+    public roomNumber: number;
+
+    public constructor(id: string, checkInDate: Date, checkOutDate: Date, guestName: string, guestEmail: string, roomNumber: number) {
+        this.id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.guestName = guestName;
+        this.guestEmail = guestEmail;
+        this.roomNumber = roomNumber;
+
+    }
 }
--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReservationService } from '../reservation/reservation.service';
 import { Reservation } from '../models/reservation';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -11,18 +11,17 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ReservationFormComponent implements OnInit {
 
-  reservationForm: FormGroup = new FormGroup({});
 
-  constructor(
-    private formBuilder: FormBuilder,
+  constructor(reservationForm: ReservationFormComponent,
+    private reservation: Reservation,
     private reservationService: ReservationService,
     private router: Router,
-    private activatedRoute: ActivatedRoute){
+    private activatedRoute: ActivatedRoute) {
 
   }
 
   ngOnInit(): void {
-    this.reservationForm = this.formBuilder.group({
+    this.reservationForm({
       checkInDate: ['', Validators.required],
       checkOutDate: ['', Validators.required],
       guestName: ['', Validators.required],
@@ -32,35 +31,40 @@ export class ReservationFormComponent implements OnInit {
 
     let id = this.activatedRoute.snapshot.paramMap.get('id')
 
-    if(id){
+    if (id) {
       let reservation = this.reservationService.getReservation(id)
 
-      if(reservation)
+      if (reservation)
         this.reservationForm.patchValue(reservation)
     }
+
+
+
+
   }
-
-  onSubmit() {
-    if(this.reservationForm.valid){
-
-      let reservation: Reservation = this.reservationForm.value;
-
-      let id = this.activatedRoute.snapshot.paramMap.get('id')
-
-      if(id){
-        // Update
-        console.log(this.reservationService.updateReservation(id, reservation))
-        
-      } else {
-        // New
-        this.reservationService.addReservation(reservation);
-        console.log(
-        this.reservationService.addReservation(reservation))  
-
-      }
-
-      this.router.navigate(['/list'])
-    }
-  }
-
 }
+
+onSubmit() {
+  if (this.reservationForm.valid) {
+
+    let reservation: Reservation = this.reservationForm.value;
+
+    let id = this.activatedRoute.snapshot.paramMap.get('id')
+
+    if (id) {
+      // Update
+      console.log(this.reservationService.updateReservation(id, reservation))
+
+    } else {
+      // New
+      this.reservationService.addReservation(reservation);
+      console.log(
+        this.reservationService.addReservation(reservation))
+
+    }
+
+    this.router.navigate(['/list'])
+  }
+}
+
+
